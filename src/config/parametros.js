1d095fbb102bf2db930aca3a3c023c6216b8da26
Romario Torres <cs360.x@hotmail.com>
2022-10-18 16:30:36 -0600

    mods

--- src/config/parametros.js
+++ src/config/parametros.js
@@ -1,5 +1,5 @@
 module.exports = {
     IP: '172.16.10.205',
-    PUERTO: process.env.NODE_ENV == 'development' ? 5555 : 7171, // se cambia a 3112 cuando ya es desarrollo
+    PUERTO: process.env.NODE_ENV == 'development' ? 5555 : 5555, // se cambia a 3112 cuando ya es desarrollo
     URL: `http://${this.IP}:${this.PUERTO}`
 }
